a98af6a51f37d8f998d635963bcf2ad36c956d29
Maxime Guillod <maxime@proteck.ch>
2017-05-04 17:19:19 +0200

    Init project & Data

---
+++ src/mcr_visitor/Launcher.java
@@ -0,0 +1,13 @@
+package mcr_visitor;
+
+/**
+ *
+ * @author Maxime Guillod
+ */
+public class Launcher {
+
+    public static void main(String[] args) {
+        System.out.println("Hello guys !");
+    }
+    
+}
